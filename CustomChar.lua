704b9d417252f94e66bc172041f53fee74eb4d65
PoopUpGamer123 <zamiko904@gmail.com>
2025-09-09 15:04:34 -0300

    CustomChar modificado pra um amigo.lua

--- CustomChar.lua
+++ CustomChar.lua
@@ -1,7 +1,7 @@
 local Tabela = {
 	PlayerPermission = {
-		["AntiBloxYT9"] = 			{Tier = 2000},
-		["AntiBloxYT"] = 			{Tier = 2000},
+		["lulucatapi_6"] = 			{Tier = 2000},
+		["lulucatapi_6"] = 			{Tier = 2000},
 		["VigorOGr"] = 				{Tier = 1},
 		["jguuh"] = 				{Tier = 1},
 		["b112014"] = 				{Tier = 1},
@@ -12,19 +12,19 @@ local Tabela = {
 	},
 	
 	CharacterTier = {
-		["Enrico"] = 					{Tier = 1,Permission = {5204134300,4035850120,1706462376}},
-		["DIO"]	=					{Locked = {5204134300}},
-		["Madara"]	=				{Locked = {5204134300}},
-		["Koku"]	=				{Locked = {5204134300}},
-		["Yori"]	=				{Permission = {5204134300,3108673472,1706462376,2987691941}},
-		["Meliodas"]	=				{Permission = {5204134300,629259254,785068132,3108673472}},
-		["Goku"]	=				{Locked = {5204134300}},
-		["TOP"]	=					{Locked = {5204134300}},
-		["Giorno"] = 					{Tier = 1050,Permission = {5204134300,785068132,3108673472,834610807}},
-		["Hakaishin"] = 				{Locked = {5204134300,128677359,785068132,629259254}},
-		["Boss"] = 					{Locked = {5204134300,36651812,4035850120}},
-		["JP6"] = 					{Tier = 1,Permission = {5204134300,2908376678,1706565385,3108673472,3162059849}},
-		["Gojo"] = 					{Tier = 1,Permission = {5204134300,2963030044}}, -- perm temp
+		["Enrico"] = 					{Tier = 1,Permission = {8253436013,4035850120,1706462376}},
+		["DIO"]	=					{Locked = {8253436013}},
+		["Madara"]	=				{Locked = {8253436013}},
+		["Koku"]	=				{Locked = {8253436013}},
+		["Yori"]	=				{Permission = {8253436013,3108673472,1706462376,2987691941}},
+		["Meliodas"]	=				{Permission = {8253436013,629259254,785068132,3108673472}},
+		["Goku"]	=				{Locked = {8253436013}},
+		["TOP"]	=					{Locked = {8253436013}},
+		["Giorno"] = 					{Tier = 1050,Permission = {8253436013,785068132,3108673472,834610807}},
+		["Hakaishin"] = 				{Locked = {8253436013,128677359,785068132,629259254}},
+		["Boss"] = 					{Locked = {8253436013,36651812,4035850120}},
+		["JP6"] = 					{Tier = 1,Permission = {8253436013,2908376678,1706565385,3108673472,3162059849}},
+		["Gojo"] = 					{Tier = 1,Permission = {8253436013,2963030044}}, -- perm temp
 	}
 
 }
